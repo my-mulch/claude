161f66d45c2557d3cf26e98350a55afbc6882a86
trumanpurnell <truman.purnell@gmail.com>
2019-10-19 01:13:50 -0700

    adds cross product via algebra templating

--- big-box/operations/linalg/cross.mjs
+++ big-box/operations/linalg/cross.mjs
@@ -8,37 +8,18 @@ export default {
     },
 
     pointwise: function (A, B, R, meta) {
-        const symbolicA = [new Array(A.type.size).fill(0)]
-        const symbolicB = [new Array(A.type.size).fill(0)]
-        const symbolicR = [new Array(A.type.size).fill(0)]
-
+        const sA = [], sB = [], sR = []
 
         for (let i = 0; i < 3; i++) {
-            symbolicA.push(Algebra.variable({
-                symbol: 'A.data',
-                index: A.header.flatIndex([i, 0]),
-                size: A.type.size
-            }))
-
-            symbolicB.push(Algebra.variable({
-                symbol: 'B.data',
-                index: B.header.flatIndex([i, 0]),
-                size: B.type.size
-            }))
-
-            symbolicR.push(Algebra.variable({
-                symbol: 'R.data',
-                index: R.header.flatIndex([i, 0]),
-                size: R.type.size
-            }))
+            sA.push(Algebra.variable({ symbol: 'A.data', index: A.header.flatIndex([i, 0]), size: A.type.size }))
+            sB.push(Algebra.variable({ symbol: 'B.data', index: B.header.flatIndex([i, 0]), size: B.type.size }))
+            sR.push(Algebra.variable({ symbol: 'R.data', index: R.header.flatIndex([i, 0]), size: R.type.size }))
         }
 
         return new Function('A, B, R', [
-            Algebra
-                .assign(symbolicR, Algebra.multiply(symbolicA, symbolicB))
-                .slice(1)
-                .join('\n'),
-
+            ...Algebra.assign(sR[0], Algebra.add(Algebra.negate(Algebra.multiply(sA[2], sB[1])), Algebra.multiply(sB[2], sA[1]))),
+            ...Algebra.assign(sR[1], Algebra.add(Algebra.negate(Algebra.multiply(sA[0], sB[2])), Algebra.multiply(sB[0], sA[2]))),
+            ...Algebra.assign(sR[2], Algebra.subtract(Algebra.multiply(sA[0], sB[1]), Algebra.multiply(sB[0], sA[1]))),
             `return R`
         ].join('\n'))
     }
