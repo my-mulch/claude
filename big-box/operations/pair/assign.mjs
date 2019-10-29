4dcc98af2808accf3fcbd8de10f0173646d6ac6b
trumanpurnell <truman.purnell@gmail.com>
2019-10-28 19:57:45 -0700

    removes array return from assign operation

--- big-box/operations/pair/assign.mjs
+++ big-box/operations/pair/assign.mjs
@@ -4,9 +4,8 @@ import PairOperation from './operation'
 export default class Assignment extends PairOperation {
     constructor(A, B, R, { region = [] }) {
         super(A.slice({ region }), B, R = A, function () {
-            return [
-                Algebra.assign(this.symbolic.variables.A, this.symbolic.variables.B),
-            ]
+            return Algebra.assign(this.symbolic.variables.A, this.symbolic.variables.B)
+            
         })
     }
 }
