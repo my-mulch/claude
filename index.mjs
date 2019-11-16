7f8fd364f546f322ba9923a22c36c6f1c81a9cc3
trumanpurnell <truman.purnell@gmail.com>
2019-11-16 01:22:44 -0800

    comments out image fetching to allow master to shine

--- index.mjs
+++ index.mjs
@@ -11,7 +11,7 @@ window.app = parmesan
 window.config = config
 
 export default (async function () {
-    app.plot(await imageCube('http://localhost:3000/Users/trumanpurnell/Pictures/11233270_1146995038650978_3611923540576057298_o.jpg'))
+    // app.plot(await imageCube('http://localhost:3000/Users/trumanpurnell/Pictures/11233270_1146995038650978_3611923540576057298_o.jpg'))
     app.plot(axes)
     // app.plot(rgb)
     app.plot(circle)
