1167e13e9845ae55a1d10b19488367557af808cf
trumanpurnell <truman.purnell@gmail.com>
2019-11-16 01:25:31 -0800

    adds sparse rgb cube

--- index.mjs
+++ index.mjs
@@ -13,7 +13,7 @@ window.config = config
 export default (async function () {
     // app.plot(await imageCube('http://localhost:3000/Users/trumanpurnell/Pictures/11233270_1146995038650978_3611923540576057298_o.jpg'))
     app.plot(axes)
-    // app.plot(rgb)
+    app.plot(rgb)
     app.plot(circle)
     app.plot(points)
 })()
