80bd085479dbbe663e5418dda5638f46d227aa93
trumanpurnell <truman.purnell@gmail.com>
2019-11-19 16:03:19 -0800

    completes adjugate operation pointwise

--- big-box/operations/linalg/adjugate.mjs
+++ big-box/operations/linalg/adjugate.mjs
@@ -1,35 +1,46 @@
+import Algebra from '../../template/algebra'
 import Determinant from './determinant'
-import Algebra from '../../../template/algebra'
-import TensorOperation from '../../operation'
+import LinearAlgebraOperation from './operation.mjs'
 import { indexTemplate } from './utils.mjs'
 
-export default class Adjugate extends TensorOperation {
+export default class Adjugate extends LinearAlgebraOperation {
     constructor(args) {
-        super(args, {
-            route: function () { return this.pointwise() },
-            resultant: function () { return Tensor.zeros(this.of) },
-            pointwise: function () {
-                const source = []
+        /** Superclass */
+        super(args)
 
-                this.determinant = new Determinant(args)
+        /** Result */
+        this.result = args.result || this.resultant()
 
-                for (let r = 0; r < this.size; r++) {
-                    for (let c = 0; c < this.size; c++) {
-                        const sign = Math.pow(-1, (r + c) % 2)
-                        const minor = Determinant.minor(indexTemplate(this.size), c, r)
-                        const determinant = this.determinant.pointwiseSource(minor)
-                        const cofactor = sign < 0 ? Algebra.negate(determinant) : determinant
+        /** Initialize */
+        if (this.of.size > 0) {
+            this.pointwiseSourceBoilerplate() // super class method
+            this.pointwiseSourceTemplate() // super class method, utilizes helpers below
+        }
 
-                        source.push(Algebra.assign(Algebra.variable({
-                            index: this.result.header.flatIndex([r, c]),
-                            symbol: 'R.data',
-                            size: this.result.type.size
-                        }), cofactor))
-                    }
-                }
-
-                return source.join('\n')
-            }
-        })
+        /** Create */
+        this.invoke = new Function('A,B,R', [this.source, 'return R'].join('\n'))
     }
+
+    /** Pointwise Implementation */
+    start() {
+        this.source = []
+        this.determinant = new Determinant(this)
+    }
+
+    inLoop() {
+        const sign = Math.pow(-1, (r + c) % 2)
+        const minor = Determinant.minor(indexTemplate(this.size), c, r)
+        const determinant = this.determinant.pointwiseSource(minor)
+        const cofactor = sign < 0 ? Algebra.negate(determinant) : determinant
+
+        this.source.push(Algebra.assign(Algebra.variable({
+            index: this.result.header.flatIndex([r, c]),
+            symbol: 'R.data',
+            size: this.result.type.size
+        }), cofactor))
+    }
+
+    finish() { this.source = this.source.join('\n') }
+
+    /** (TODO) Symbolic Implementation */
 }
---
+++ big-box/operations/linalg/operation.mjs
@@ -0,0 +1,34 @@
+import Tensor from '../../tensor'
+import TensorOperation from '../operation'
+
+export default class LinearAlgebraOperation extends TensorOperation {
+    constructor(args) {
+        super(args)
+
+        /** Dimensions */
+        this.rows = this.of.shape[0]
+        this.cols = this.with.shape[1]
+        this.like = this.of.shape[1]
+        this.size = this.rows
+    }
+
+    /** Resultant Tensor */
+    resultant() { return Tensor.zeros(this.of) }
+
+    /** Pointwise Source Implementation */
+    pointwiseSourceBoilerplate() { }
+    pointwiseSourceTemplate() {
+        this.start()
+
+        for (let r = 0; r < this.size; r++)
+            for (let c = 0; c < this.size; c++)
+                this.inLoop()
+
+        this.finish()
+    }
+
+    /** (TODO) Symbolic Source Implementation */
+    symbolicSourceBoilerplate() { }
+    symbolicSourceTemplate() { }
+}
+
