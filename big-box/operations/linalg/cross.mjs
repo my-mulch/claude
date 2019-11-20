c884df0d0db2ad9533415b68d8c617fa5169db8a
trumanpurnell <truman.purnell@gmail.com>
2019-11-19 16:14:17 -0800

    completes crossProduct operation pointwise

--- big-box/operations/linalg/cross.mjs
+++ big-box/operations/linalg/cross.mjs
@@ -1,27 +1,43 @@
-import Tensor from '../../tensor'
 import Algebra from '../../template/algebra'
-import LinearAlgebraOperation from '../operation'
+import LinearAlgebraOperation from './operation.mjs'
 
 export default class CrossProduct extends LinearAlgebraOperation {
     constructor(args) {
-        super(args, {
-            route: function () { return this.pointwise() },
-            resultant: function () { return Tensor.zeros({ shape: [3, 1], type: this.of.type }) },
-            pointwise: function () {
-                const A = [], B = [], R = []
+        /** Superclass */
+        super(args)
 
-                for (let i = 0; i < 3; i++) {
-                    A.push(Algebra.variable({ symbol: 'A.data', index: this.of.header.flatIndex([i, 0]), size: this.of.type.size }))
-                    B.push(Algebra.variable({ symbol: 'B.data', index: this.with.header.flatIndex([i, 0]), size: this.with.type.size }))
-                    R.push(Algebra.variable({ symbol: 'R.data', index: this.result.header.flatIndex([i, 0]), size: this.result.type.size }))
-                }
+        /** Result */
+        this.result = args.result || this.resultant()
 
-                return [
-                    Algebra.assign(R[0], Algebra.add(Algebra.negate(Algebra.multiply(A[2], B[1])), Algebra.multiply(B[2], A[1]))),
-                    Algebra.assign(R[1], Algebra.add(Algebra.negate(Algebra.multiply(A[0], B[2])), Algebra.multiply(B[0], A[2]))),
-                    Algebra.assign(R[2], Algebra.subtract(Algebra.multiply(A[0], B[1]), Algebra.multiply(B[0], A[1]))),
-                ]
-            }
-        })
+        /** Initialize */
+        if (this.of.size > 0) {
+            this.pointwiseSourceBoilerplate() // super class method
+            this.pointwiseSourceTemplate() 
+        }
+
+        /** Create */
+        this.invoke = new Function('A,B,R', [this.source, 'return R'].join('\n'))
     }
+
+    /** Resultant Tensor */
+    resultant() { return Tensor.zeros({ shape: [3, 1], type: this.of.type }) }
+
+    /** Pointwise Implementation */
+    pointwiseSourceTemplate() {
+        const A = [], B = [], R = []
+
+        for (let i = 0; i < 3; i++) {
+            A.push(Algebra.variable({ symbol: 'A.data', index: this.of.header.flatIndex([i, 0]), size: this.of.type.size }))
+            B.push(Algebra.variable({ symbol: 'B.data', index: this.with.header.flatIndex([i, 0]), size: this.with.type.size }))
+            R.push(Algebra.variable({ symbol: 'R.data', index: this.result.header.flatIndex([i, 0]), size: this.result.type.size }))
+        }
+
+        this.source = [
+            Algebra.assign(R[0], Algebra.add(Algebra.negate(Algebra.multiply(A[2], B[1])), Algebra.multiply(B[2], A[1]))),
+            Algebra.assign(R[1], Algebra.add(Algebra.negate(Algebra.multiply(A[0], B[2])), Algebra.multiply(B[0], A[2]))),
+            Algebra.assign(R[2], Algebra.subtract(Algebra.multiply(A[0], B[1]), Algebra.multiply(B[0], A[1]))),
+        ].join('\n')
+    }
+
+    /** (TODO) Symbolic Implementation */
 }
