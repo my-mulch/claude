64cb3c0a976b0a72fe35ffd101c22a9b6036de5b
trumanpurnell <truman.purnell@gmail.com>
2019-12-06 00:15:06 -0800

    rendering points example with new gl interface

--- index.mjs
+++ index.mjs
@@ -1,28 +1,38 @@
 
 
-const program = webgl
+webgl
     .program({
         canvas: app.canvas,
         vertexSource: await io.txtread('./webgl/click/shader.vert'),
         fragmentSource: await io.txtread('./webgl/click/shader.frag'),
     }).do(function () {
-        /** Variables */
+        /** Attribute Locations */
         const aPosIndex = this.attributes.a_Position.attributeLocation
         const aSizeIndex = this.attributes.a_PointSize.attributeLocation
 
-        /** GPU Data */
-        this.context.vertexAttrib3f(aPosIndex, 0.2, -0.1, .3);
-        this.context.vertexAttrib1f(aSizeIndex, 80);
+        /** Send to GPU */
+        this.context.vertexAttrib1f(aSizeIndex, 80)
 
-    }).do(function () {
         /** Clear color */
         this.context.clearColor(0., 0.0, 0.0, 1.0)
 
-        /** Clear <canvas> to set color */
-        this.context.clear(this.context.COLOR_BUFFER_BIT)
+        /** Mouse Clicks */
+        const points = []
+        app.canvas.addEventListener('mousedown', function (event) {
+            const rect = event.target.getBoundingClientRect()
 
-        /** Draw */
-        this.context.drawArrays(this.context.POINTS, 0, 2)
+            const x = ((event.clientX - rect.left) - app.canvas.width / 2) / (app.canvas.width / 2)
+            const y = (app.canvas.height / 2 - (event.clientY - rect.top)) / (app.canvas.height / 2)
+
+            points.push(x, y)
+
+            this.context.clear(this.context.COLOR_BUFFER_BIT)
+
+            for (let i = 0; i < points.length; i += 2) {
+                this.context.vertexAttrib3f(aPosIndex, points[i], points[i + 1], 0.0)
+                this.context.drawArrays(this.context.POINTS, 0, 1);
+            }
+        }.bind(this))
     })
 
 
@@ -30,29 +40,3 @@ const program = webgl
 
 
 
-
-
-
-// var g_points = []; // The array for the position of a mouse press
-// function click(ev, gl, canvas, a_Position) {
-//     var x = ev.clientX; // x coordinate of a mouse pointer
-//     var y = ev.clientY; // y coordinate of a mouse pointer
-//     var rect = ev.target.getBoundingClientRect();
-
-//     x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
-//     y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
-//     // Store the coordinates to g_points array
-//     g_points.push(x); g_points.push(y);
-
-//     // Clear <canvas>
-//     gl.clear(gl.COLOR_BUFFER_BIT);
-
-//     var len = g_points.length;
-//     for (var i = 0; i < len; i += 2) {
-//         // Pass the position of a point to a_Position variable
-//         gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
-
-//         // Draw
-//         gl.drawArrays(gl.POINTS, 0, 1);
-//     }
-// }
