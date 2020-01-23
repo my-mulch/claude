ac8029357ec4f647d2f3af42236a46ecc444381c
trumanpurnell <truman.purnell@gmail.com>
2020-01-22 18:02:12 -0800

    first gradient descent example

--- index.mjs
+++ index.mjs
@@ -4,6 +4,54 @@ app.engine.session(
     await io.txtread('./resources/shaders/rgb/shader.frag'),
 )
 
+let weight = 0.5
+const goal = 0.8
+const input = 0.5
+
+function loss(weight, input, goal) {
+    return (weight * input - goal) ** 2
+}
+
+function grad(weight, input, goal) {
+    return 2 * input * (weight * input - goal)
+}
+
+const weights = bb.linspace(-5, 5, 10000)
+const outputs = bb.zeros([10000, 3])
+const grads = bb.zeros([10000, 3])
+
+for (let i = 0, j = 0; i < weights.data.length; i++ , j += 3) {
+    outputs.data[j + 0] = weights.data[i + 0]
+    outputs.data[j + 1] = loss(weights.data[i + 0], input, goal)
+
+    grads.data[j + 0] = weights.data[i + 0]
+    grads.data[j + 1] = grad(weights.data[i + 0], input, goal)
+}
+
+
+const guesses = bb.zeros([22, 3])
+
+// Learning
+const eps = 1e-6
+let i = 0
+
+while (true) {
+    const error = loss(weight, input, goal)
+    const grad = input * (weight * input - goal)
+
+    guesses.data[i++] = weight * input
+    guesses.data[i++] = 0
+    guesses.data[i++] = 0
+
+    if (error < eps)
+        break
+
+    console.log(`Error: ${error} Prediction: ${weight * input}`)
+
+    weight -= grad
+}
+
+
 
 app.plot({
     vertices: bb.tensor([
@@ -30,23 +78,36 @@ app.plot({
     mode: app.engine.context.LINES,
 })
 
-
-const cylinder = bb.tensor(new Cylinder().vertices).reshape(-1, 3)
-
 app.plot({
-    vertices: cylinder,
-    colors: bb.vstack(bb.ones([1, cylinder.header.shape[0]]), bb.zeros([2, cylinder.header.shape[0]])).T().reshape(-1, 3),
-    sizes: bb.ones(cylinder.header.shape),
-    mode: app.engine.context.TRIANGLES
+    vertices: outputs,
+    colors: bb.ones([10000, 3]),
+    sizes: bb.ones([10000, 1]),
+    mode: app.engine.context.POINTS
 })
 
-const cone = bb.tensor(new Cone([1, 0, 0], [0.5, 0, 0]).vertices).reshape(-1, 3)
+app.plot({
+    vertices: guesses,
+
+    colors: bb.vstack(
+        bb.zeros([1, 22]),
+        bb.ones([1, 22]),
+        bb.zeros([1, 22]),
+    ).T().reshape(-1, 3),
+
+    sizes: bb.tensor(new Float32Array(22).fill(10)).reshape(22, 1),
+    mode: app.engine.context.POINTS
+})
+
 
 app.plot({
-    vertices: cone,
-    colors: bb.vstack(bb.zeros([2, cone.header.shape[0]]), bb.ones([1, cone.header.shape[0]])).T().reshape(-1, 3),
-    sizes: bb.ones(cone.header.shape),
-    mode: app.engine.context.TRIANGLES
+    vertices: grads,
+    colors: bb.vstack(
+        bb.ones([1, 10000]),
+        bb.zeros([1, 10000]),
+        bb.zeros([1, 10000]),
+    ).T().reshape(-1, 3),
+    sizes: bb.ones([10000, 1]),
+    mode: app.engine.context.POINTS
 })
 
 app.render()
